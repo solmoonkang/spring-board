e9c54a6a42615cef80383588948caae7d8715a51
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-12 20:31:48 +0900

    feat: 게시글 레포지토리 구현

---
+++ src/main/java/kr/co/noticeboard/domain/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package kr.co.noticeboard.domain.repository;
+
+import kr.co.noticeboard.domain.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
