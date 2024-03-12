c4dca232a20bd2db687750c35aa592dcc88417ae
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-12 20:32:03 +0900

    feat: 게시글 생성 DTO 구현

---
+++ src/main/java/kr/co/noticeboard/domain/dto/request/PostReqDTO.java
@@ -0,0 +1,23 @@
+package kr.co.noticeboard.domain.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+public class PostReqDTO {
+
+    @Getter
+    @Builder
+    @AllArgsConstructor(access = AccessLevel.PROTECTED)
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class CREATE {
+
+        @NotBlank(message = "게시글 제목을 입력해주세요.")
+        private String title;
+
+        @NotBlank(message = "게시글 작성자를 입력해주세요.")
+        private String name;
+
+        @NotBlank(message = "게시글 본문을 입력해주세요.")
+        private String content;
+    }
+}
