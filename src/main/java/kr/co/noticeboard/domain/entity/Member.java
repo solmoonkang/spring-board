85636c05ddb42803542b50639dd228a54da48f61
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-13 22:25:03 +0900

    feat: 사용자 엔티티 구현

---
+++ src/main/java/kr/co/noticeboard/domain/entity/Member.java
@@ -0,0 +1,28 @@
+package kr.co.noticeboard.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tbl_members")
+@AttributeOverride(
+        name = "id",
+        column = @Column(name = "member_id", length = 4)
+)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member extends BaseEntity {
+
+    @Column(name = "email", length = 30, nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "name", length = 30)
+    private String name;
+
+    @Builder
+    public Member(String email, String name) {
+        this.email = email;
+        this.name = name;
+    }
+}
