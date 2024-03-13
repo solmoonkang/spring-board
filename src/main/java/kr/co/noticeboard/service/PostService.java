1bd282781f219e4ffb00f2d2639eb7aa94fe3a73
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-14 00:15:17 +0900

    feat: 게시글 조회 로직 구현

--- src/main/java/kr/co/noticeboard/service/PostService.java
+++ src/main/java/kr/co/noticeboard/service/PostService.java
@@ -2,6 +2,7 @@ package kr.co.noticeboard.service;
 
 import jakarta.persistence.EntityNotFoundException;
 import kr.co.noticeboard.domain.dto.request.PostReqDTO;
+import kr.co.noticeboard.domain.dto.response.PostResDTO;
 import kr.co.noticeboard.domain.entity.Member;
 import kr.co.noticeboard.domain.entity.Post;
 import kr.co.noticeboard.domain.repository.MemberRepository;
@@ -9,6 +10,9 @@ import kr.co.noticeboard.domain.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -24,7 +28,12 @@ public class PostService {
         final Member member = memberRepository.findById(memberId)
                 .orElseThrow(() -> new EntityNotFoundException("회원을 찾을 수 없습니다."));
 
-        final Post post = Post.createPost(member, create);
+        final Post post = Post.toPostEntity(member, create);
         postRepository.save(post);
+    }
+
+    public List<PostResDTO.READ> findAllPost() {
+        final List<Post> findPost = postRepository.findAll();
+        return findPost.stream().map(Post::toReadDto).collect(Collectors.toList());
     }
 }
