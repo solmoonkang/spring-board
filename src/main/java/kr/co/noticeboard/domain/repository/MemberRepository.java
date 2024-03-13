629b26374209596631a0f60771b8d362f4c3c93b
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-13 23:05:05 +0900

    feat: 회원 이름으로 조회하는 로직 구현

--- src/main/java/kr/co/noticeboard/domain/repository/MemberRepository.java
+++ src/main/java/kr/co/noticeboard/domain/repository/MemberRepository.java
@@ -3,5 +3,9 @@ package kr.co.noticeboard.domain.repository;
 import kr.co.noticeboard.domain.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    List<Member> findMemberByName(String name);
 }
