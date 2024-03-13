b69a2d0f5e6ff250fc09f3832c50b9ac88adbf47
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-13 23:43:42 +0900

    feat: 회원 조회 컨트롤러 구현

--- src/main/java/kr/co/noticeboard/controller/MemberController.java
+++ src/main/java/kr/co/noticeboard/controller/MemberController.java
@@ -1,14 +1,14 @@
 package kr.co.noticeboard.controller;
 
 import kr.co.noticeboard.domain.dto.request.MemberReqDTO;
+import kr.co.noticeboard.domain.dto.response.MemberResDTO;
 import kr.co.noticeboard.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/members")
@@ -21,5 +21,10 @@ public class MemberController {
     public ResponseEntity<Void> createMember(@RequestBody @Validated MemberReqDTO.CREATE create) {
         memberService.createMember(create);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{name}")
+    public ResponseEntity<List<MemberResDTO.READ>> findMemberByName(@PathVariable String name) {
+        return ResponseEntity.ofNullable(memberService.findMemberByName(name));
     }
 }
