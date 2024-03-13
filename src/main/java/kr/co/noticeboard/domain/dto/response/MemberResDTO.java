a0efe799620841e791e4d809f1d238870ca1418e
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-13 23:05:21 +0900

    feat: 회원 조회 DTO 구현

---
+++ src/main/java/kr/co/noticeboard/domain/dto/response/MemberResDTO.java
@@ -0,0 +1,15 @@
+package kr.co.noticeboard.domain.dto.response;
+
+import lombok.*;
+
+public class MemberResDTO {
+
+    @Getter
+    @Builder
+    @AllArgsConstructor(access = AccessLevel.PROTECTED)
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class READ {
+
+        private String name;
+    }
+}
