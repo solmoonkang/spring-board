0948b1dcb510538651ad260692e4201fc750f944
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-13 22:25:14 +0900

    feat: 게시글 엔티티 연관관계 설정

--- src/main/java/kr/co/noticeboard/domain/entity/Post.java
+++ src/main/java/kr/co/noticeboard/domain/entity/Post.java
@@ -5,31 +5,43 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "tbl_posts")
+@AttributeOverride(
+        name = "id",
+        column = @Column(name = "post_id", length = 4)
+)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Post extends BaseEntity {
-
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
 
     @Column(name = "title", length = 50)
     private String title;
 
-    @Column(name = "name", length = 30)
-    private String name;
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
 
     @Column(name = "content", length = 500)
     private String content;
 
+    @OneToMany(
+            mappedBy = "post",
+            cascade = CascadeType.REMOVE,
+            fetch = FetchType.LAZY
+    )
+    private List<Comment> comments = new ArrayList<>();
+
     @Builder
-    public Post(Long id,
-                String title,
-                String name,
-                String content) {
-        this.id = id;
+    public Post(String title,
+                Member member,
+                String content,
+                List<Comment> comments) {
         this.title = title;
-        this.name = name;
+        this.member = member;
         this.content = content;
+        this.comments = comments;
     }
 }
