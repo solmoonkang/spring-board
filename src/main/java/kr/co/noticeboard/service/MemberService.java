75c7ff85f39136215a9d9b8fa9c5fe64bb5ba773
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-13 23:05:11 +0900

    feat: 회원 이름으로 조회하는 로직 구현

--- src/main/java/kr/co/noticeboard/service/MemberService.java
+++ src/main/java/kr/co/noticeboard/service/MemberService.java
@@ -1,11 +1,15 @@
 package kr.co.noticeboard.service;
 
 import kr.co.noticeboard.domain.dto.request.MemberReqDTO;
+import kr.co.noticeboard.domain.dto.response.MemberResDTO;
 import kr.co.noticeboard.domain.entity.Member;
 import kr.co.noticeboard.domain.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -16,8 +20,12 @@ public class MemberService {
 
     @Transactional
     public void createMember(MemberReqDTO.CREATE create) {
-        final Member member = Member.createMember(create);
+        final Member member = Member.toMemberEntity(create);
         memberRepository.save(member);
     }
 
+    public List<MemberResDTO.READ> getMemberByName(String name) {
+        final List<Member> findMember = memberRepository.findMemberByName(name);
+        return findMember.stream().map(Member::toReadDto).collect(Collectors.toList());
+    }
 }
