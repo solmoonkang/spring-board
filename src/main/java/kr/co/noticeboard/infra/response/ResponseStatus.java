2bf0a19388b2e81a7ed646b8dde68093738defaf
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-14 18:01:52 +0900

    feat: 회원, 게시글, 댓글을 찾을 수 없을 때 예외 상태 추가

--- src/main/java/kr/co/noticeboard/infra/response/ResponseStatus.java
+++ src/main/java/kr/co/noticeboard/infra/response/ResponseStatus.java
@@ -13,10 +13,18 @@ public enum ResponseStatus {
     SUCCESS_EXECUTE(HttpStatus.OK, "✅ [SUCCESS] 요청이 성공적으로 처리되었습니다."),
     SUCCESS_CREATED(HttpStatus.CREATED, "✅ [SUCCESS] 리소스가 성공적으로 생성되었습니다."),
 
+
     FAIL_BAD_REQUEST(HttpStatus.BAD_REQUEST, "❎ [ERROR] 잘못된 요청입니다. 요청 형식을 확인해주세요."),
     FAIL_UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "❎ [ERROR] 인증되지 않은 사용자 입니다."),
     FAIL_NOT_FOUND(HttpStatus.NOT_FOUND, "❎ [ERROR] 요청한 리소스를 찾을 수 없습니다."),
-    FAIL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "❎ [ERROR] 서버 내부 오류가 발생했습니다. 잠시 후 다시 시도해주세요.");
+    FAIL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "❎ [ERROR] 서버 내부 오류가 발생했습니다. 잠시 후 다시 시도해주세요."),
+
+
+    FAIL_MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "❎ [ERROR] 요청한 회원을 찾을 수 없습니다."),
+
+    FAIL_POST_NOT_FOUND(HttpStatus.NOT_FOUND, "❎ [ERROR] 요청한 게시글을 찾을 수 없습니다."),
+
+    FAIL_COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "❎ [ERROR] 요청한 댓글을 찾을 수 없습니다.");
 
     private HttpStatus status;
 
