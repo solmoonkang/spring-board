8b30f192a4a568692efb37f69f58c80d104cdf8f
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-14 18:02:09 +0900

    refactor: 가독성 향상을 위해 메소드명 수정

--- src/main/java/kr/co/noticeboard/service/PostService.java
+++ src/main/java/kr/co/noticeboard/service/PostService.java
@@ -1,12 +1,13 @@
 package kr.co.noticeboard.service;
 
-import jakarta.persistence.EntityNotFoundException;
 import kr.co.noticeboard.domain.dto.request.PostReqDTO;
 import kr.co.noticeboard.domain.dto.response.PostResDTO;
 import kr.co.noticeboard.domain.entity.Member;
 import kr.co.noticeboard.domain.entity.Post;
 import kr.co.noticeboard.domain.repository.MemberRepository;
 import kr.co.noticeboard.domain.repository.PostRepository;
+import kr.co.noticeboard.infra.exception.NotFoundException;
+import kr.co.noticeboard.infra.response.ResponseStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,8 +26,9 @@ public class PostService {
 
     @Transactional
     public void createPost(Long memberId, PostReqDTO.CREATE create) {
+
         final Member member = memberRepository.findById(memberId)
-                .orElseThrow(() -> new EntityNotFoundException("회원을 찾을 수 없습니다."));
+                .orElseThrow(() -> new NotFoundException(ResponseStatus.FAIL_MEMBER_NOT_FOUND));
 
         final Post post = Post.toPostEntity(member, create);
 
@@ -34,43 +36,51 @@ public class PostService {
     }
 
     public List<PostResDTO.READ> findAllPost() {
+
         final List<Post> findPost = postRepository.findAll();
 
         return findPost.stream().map(Post::toReadDto).collect(Collectors.toList());
     }
 
     public PostResDTO.DETAIL findPostById(Long postId) {
+
         final Post findPost = findPostOrThrow(postId);
 
         return findPost.toReadDetailDto();
     }
 
     @Transactional
-    public void updatePost(Long postId, Long memberId, PostReqDTO.UPDATE update) {
+    public void updatePost(Long postId,
+                           Long memberId,
+                           PostReqDTO.UPDATE update) {
+
         final Post updatePost = findPostOrThrow(postId);
 
-        checkMemberAuthorization(updatePost, memberId);
+        verifyMemberExistsOrThrow(updatePost, memberId);
 
         updatePost.updatePost(update);
     }
 
     @Transactional
     public void deletePost(Long postId, Long memberId) {
+
         final Post deletePost = findPostOrThrow(postId);
 
-        checkMemberAuthorization(deletePost, memberId);
+        verifyMemberExistsOrThrow(deletePost, memberId);
 
         deletePost.markAsDeleted();
     }
 
-    public void checkMemberAuthorization(Post post, Long memberId) {
+    public void verifyMemberExistsOrThrow(Post post, Long memberId) {
+
         if (!post.getMember().getId().equals(memberId)) {
-            throw new EntityNotFoundException("회원을 찾을 수 없습니다.");
+            throw new NotFoundException(ResponseStatus.FAIL_MEMBER_NOT_FOUND);
         }
     }
 
     private Post findPostOrThrow(Long postId) {
+
         return postRepository.findById(postId)
-                .orElseThrow(() -> new EntityNotFoundException("게시글을 찾을 수 없습니다."));
+                .orElseThrow(() -> new NotFoundException(ResponseStatus.FAIL_POST_NOT_FOUND));
     }
 }
