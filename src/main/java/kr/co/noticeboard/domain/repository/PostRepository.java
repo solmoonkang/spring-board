56dba6d3f3243c836008ae7a073dd556451c8f21
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-14 18:22:49 +0900

    feat: 게시글 목록 조회 시 최신글이 우선적으로 조회되도록 구현

--- src/main/java/kr/co/noticeboard/domain/repository/PostRepository.java
+++ src/main/java/kr/co/noticeboard/domain/repository/PostRepository.java
@@ -2,8 +2,14 @@ package kr.co.noticeboard.domain.repository;
 
 import kr.co.noticeboard.domain.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    @Query("SELECT p FROM Post p ORDER BY p.createdAt DESC")
+    List<Post> findAllPostsOrderByCreatedAtDesc();
 }
