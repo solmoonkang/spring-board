169f70e5bd07f066eeb43ca46e20c3905cbde3dc
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-15 11:04:59 +0900

    feat: 이메일 검증 로직 구현

--- src/main/java/kr/co/noticeboard/domain/repository/MemberRepository.java
+++ src/main/java/kr/co/noticeboard/domain/repository/MemberRepository.java
@@ -3,9 +3,11 @@ package kr.co.noticeboard.domain.repository;
 import kr.co.noticeboard.domain.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    List<Member> findMemberByName(String name);
+    boolean existsByEmail(String email);
+
+    Optional<Member> findMemberByEmail(String email);
 }
