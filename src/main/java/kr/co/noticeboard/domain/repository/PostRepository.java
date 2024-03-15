63d828cc9d0aedf2539244007513db180b71d851
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-15 17:13:46 +0900

    feat: 게시글 단 건 조회 시 발생하는 N+1 문제 해결

--- src/main/java/kr/co/noticeboard/domain/repository/PostRepository.java
+++ src/main/java/kr/co/noticeboard/domain/repository/PostRepository.java
@@ -1,16 +1,10 @@
 package kr.co.noticeboard.domain.repository;
 
 import kr.co.noticeboard.domain.entity.Post;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
-
-    @Query("SELECT p FROM Post p ORDER BY p.createdAt DESC")
-    Page<Post> findAllPostsOrderByCreatedAtDesc(Pageable pageable);
 }
